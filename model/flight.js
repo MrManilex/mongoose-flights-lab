3cd1de8bbb052a5b9ded0798261f3d4a07761f08
WangManLex <alexandermaniwang99@gmail.com>
2021-10-28 20:34:11 -0400

    add flight model

---
+++ model/flight.js
@@ -0,0 +1,22 @@
+import { Mongoose } from "mongoose"
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: {type: String, enum: ['American', 'Southwest', 'United']},
+  airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
+  flightNo: {type: Number, min: 10, max: 9999},
+  departs: {type: Date,
+    default: function(){
+      let date = new Date()
+      date.setFullYear(date.getFullYear() + 1)
+      return date
+    }
+  }
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export{
+  Flight
+}
